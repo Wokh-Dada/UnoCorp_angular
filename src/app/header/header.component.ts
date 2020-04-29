d5aab93c9bd4f4a9eec77a35bded1df26b535819
gezikhanov <gezihanov95@mail.ru>
2020-04-29 14:01:22 +0300

    fixed bootstrap link and nav

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,8 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  // @ts-ignore
+  menu = [
+    {
+      nameLink: "Home",
+      complited: true,
+      popup: [
+        {popupItemName: "Homepage Main", complited: false},
+        {popupItemName: "Homepage Finance", complited: true},
+        {popupItemName: "Homepage App", complited: false},
+        {popupItemName: "Homepage Startup", complited: false},
+        {popupItemName: "Homepage Business", complited: false},
+        {popupItemName: "Homepage SaaS", complited: false},
+        {popupItemName: "Homepage Portfolio", complited: false},
+        {popupItemName: "Homepage Consulting", complited: false},
+        {popupItemName: "Homepage Cprporate", complited: false}
 
-  constructor() { }
+      ]
+    },
+    {nameLink: "About", complited: false},
+    {nameLink: "Services", complited: false},
+    {nameLink: "Pricing", complited: false},
+    {nameLink: "Blog", complited: false}
+  ]
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
